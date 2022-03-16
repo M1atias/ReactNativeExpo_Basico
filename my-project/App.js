c281d5ed980e5668394c1356f0bdeda042ab1979
M1atias <maty.f.molina4a@gmail.com>
2022-03-16 04:53:00 -0300

    add: se empezo con la estilización del loguin

--- my-project/App.js
+++ my-project/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React from'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello</Text>
+      <Text style={styles.subtitle}>Sign In to your account</Text>
+      <TextInput 
+      placeholder='matias@gmail.com'
+      style={styles.textinput}/>
+      <TextInput 
+      placeholder='password'
+      style={styles.textinput}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontSize:80,
+    color:'#34434D',
+    fontWeight:'bold',
+  },
+  subtitle:{
+    fontSize:20,
+    color:'gray',
+  },
+  textinput:{
+    padding:10,
+    paddingStart:30,
+    width:'80%',
+    marginTop:20,
+    borderRadius:30,
+    height:50,
+    backgroundColor:'#fff'
+  }
 });
